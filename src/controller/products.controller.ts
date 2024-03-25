354a38f2617d8f3619556391df55165cd4b2060d
Mateus Godinho <mateusmrad@gmail.com>
2024-03-25 19:39:24 -0300

    arrumando o lint, typando a funcao do controller

--- src/controller/products.controller.ts
+++ src/controller/products.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import productsService from '../services/products.service';
 import { httpMap } from '../utils/httpMap';
 
-async function createProd(req: Request, res: Response) {
+async function createProd(req: Request, res: Response): Promise<Response> {
   const { id, name, price, userId } = req.body;
   const { data, status } = await productsService.createProd({ id, name, price, userId });
   return res.status(httpMap(status)).json(data);
