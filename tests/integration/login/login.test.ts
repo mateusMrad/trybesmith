efe658ab4c955e3d150fcd6dfa3578a857145dfb
Mateus Godinho <mateusmrad@gmail.com>
2024-03-26 11:52:58 -0300

    testes, requisito 04

--- tests/integration/login/login.test.ts
+++ tests/integration/login/login.test.ts
@@ -1,10 +1,25 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../../../src/app';
 
 chai.use(chaiHttp);
 
 describe('POST /login', function () { 
   beforeEach(function () { sinon.restore(); });
-
+  it('retorna um erro de status 400 se nao for enviado password', async function () {
+    const response = await chai.request(app).post('/login').send({ username: 'usuario' });
+    expect(response).to.have.status(400);
+    expect(response.body).to.have.property('message').eq('"username" and "password" are required');
+  });
+  it('retorna um erro de status 400 se nao for enviado username', async function () {
+    const response = await chai.request(app).post('/login').send({ password: '555' });
+    expect(response).to.have.status(400);
+    expect(response.body).to.have.property('message').eq('"username" and "password" are required');
+  });
+  it('retorna um erro de status 401 se nao for enviado parametro valido', async function () {
+    const response = await chai.request(app).post('/login').send({username: 'usuario', password: '555' });
+    expect(response).to.have.status(401);
+    expect(response.body).to.have.property('message').eq('Username or password invalid');
+  })
 });
